0add8b80f669631e5879f23f07eab112bf1621c8
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-04-07 17:40:26 +0200

    Update entity.py

--- custom_components/alarms_and_reminders/entity.py
+++ custom_components/alarms_and_reminders/entity.py
@@ -8,8 +8,13 @@ class AlarmReminderEntity(Entity):
         self.hass = hass
         self.item_id = item_id
         self.data = data
-        self._attr_name = f"{'Alarm' if data['is_alarm'] else 'Reminder'} {item_id}"
+        self._attr_name = data.get("name", item_id)
         self._attr_unique_id = item_id
+
+    @property
+    def name(self):
+        """Return the name of the entity."""
+        return self._attr_name
 
     @property
     def state(self):
@@ -19,4 +24,13 @@ class AlarmReminderEntity(Entity):
     @property
     def extra_state_attributes(self):
         """Return the extra state attributes."""
-        return self.data
+        return {
+            "scheduled_time": self.data["scheduled_time"].isoformat(),
+            "message": self.data["message"],
+            "repeat": self.data["repeat"],
+            "repeat_days": self.data["repeat_days"],
+            "status": self.data["status"],
+            "satellite": self.data["satellite"],
+            "media_players": self.data["media_players"],
+            "is_alarm": self.data["is_alarm"]
+        }
