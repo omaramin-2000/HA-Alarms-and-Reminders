9033efea9e58ba532aea83d8aa3c1733f1cb4432
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-05-26 18:48:21 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="ha-alarms-and-reminders",
+    version="0.1.0",
+    packages=find_namespace_packages(include=['custom_components.*']),
+    install_requires=[
+        'voluptuous>=0.13.1',
+        'homeassistant>=2024.3.3',
+    ],
+)
