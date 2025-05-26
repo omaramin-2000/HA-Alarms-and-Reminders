d464eff39f3daac0b9f99fb615beb648adc5d500
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-05-26 18:28:36 +0300

    Delete test_basic.py

--- tests/test_basic.py
+++
@@ -1,5 +0,0 @@
-"""Basic tests for the Alarms and Reminders integration."""
-
-def test_import():
-    """Test that the integration can be imported."""
-    import custom_components.alarms_and_reminders
