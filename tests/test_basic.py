875791eb9b2735dadd70bb5ede0e37048f2dab37
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-05-26 18:26:42 +0300

    Create test_basic.py

---
+++ tests/test_basic.py
@@ -0,0 +1,5 @@
+"""Basic tests for the Alarms and Reminders integration."""
+
+def test_import():
+    """Test that the integration can be imported."""
+    import custom_components.alarms_and_reminders
