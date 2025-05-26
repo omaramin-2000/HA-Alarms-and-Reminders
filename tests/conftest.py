f565b310333a23c360d8df2b9c2ba736d14067b6
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-05-26 18:58:24 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 """Common fixtures for testing."""
+import os
 import pytest
 
 pytest_plugins = "pytest_homeassistant_custom_component"
@@ -7,5 +8,6 @@ pytest_plugins = "pytest_homeassistant_custom_component"
 @pytest.fixture(autouse=True)
 async def auto_enable_custom_integrations(hass):
     """Enable custom integrations in Home Assistant."""
+    hass.config.components.add("alarms_and_reminders")
     await hass.async_start()
     yield
