062e4f08bd28b5a6345e153e0f043d006dfe3f16
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-05-26 18:53:01 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
-"""Provide common pytest fixtures."""
+"""Common fixtures for testing."""
 import pytest
-from unittest.mock import patch
 
 pytest_plugins = "pytest_homeassistant_custom_component"
 
 
 @pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations):
+async def auto_enable_custom_integrations(hass):
     """Enable custom integrations in Home Assistant."""
+    await hass.async_start()
     yield
