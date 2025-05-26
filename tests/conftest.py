7a150956e17976dd7c4de0e7ee6011f6a70367b3
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-05-26 19:10:07 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,16 @@ pytest_plugins = "pytest_homeassistant_custom_component"
 @pytest.fixture(autouse=True)
 async def auto_enable_custom_integrations(hass):
     """Enable custom integrations in Home Assistant."""
+    # Register your component's domain in hass.data
+    hass.data["custom_components"] = {
+        "alarms_and_reminders": {
+            "name": "Alarms and Reminders",
+            "domain": "alarms_and_reminders",
+        }
+    }
+
+    # Add component to config
     hass.config.components.add("alarms_and_reminders")
+
     await hass.async_start()
     yield
